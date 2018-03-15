87f59bf1e43805ce2b01b5d4723557a2ab080e57
david <ostermann.david@gmail.com>
2018-03-15 13:03:35 +0100

    add backend

--- back/models/db.js
+++ back/models/db.js
@@ -1,9 +1,7 @@
 const { Client } = require('pg');
 const db = new Client({
-  connectionString:
-    process.env.DATABASE_URL ||
-    "postgres://ncukftaxxtcrks:a0169551ae65e788441208c502d1e95cf3e4566202a850bb49e699e0cca92363@ec2-54-75-239-237.eu-west-1.compute.amazonaws.com:5432/ddv5e22n1vfv0"
-});
+  connectionString: process.env.DATABASE_URL || 'postgres://dost:changeme@localhost:5432/trellodb'
+})
 
 db.connect((err) => {
   if (err) {
